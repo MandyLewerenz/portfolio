628a428c8f33badf39c8e713c16d9192f1cf72ee
Mandy Lewerenz <mandy.lwrz@gmail.com>
2023-11-02 23:05:59 +0100

    remove sidebar

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,8 +1,8 @@
 <template>
-  <navbarComponent />
-  <sidebarComponent />
 
-  <div class="sections-wrapper">
+  <navbarComponent />
+
+  <div ref="content" class="sections-wrapper" data-bs-spy="scroll" data-bs-target=".navbar-light">
 
     <section1 />
     <section2 />
@@ -12,27 +12,24 @@
 </template>
 
 <script>
-  import '@/css/styles.scss';
-  import '@/css/navbar.scss';
-  import '@/css/sidebar.scss';
-  import '@/css/character.scss';
-  import '@/css/profile.scss';
-  import '@/css/projects.scss';
+import '@/css/styles.scss';
+import '@/css/navbar.scss';
+import '@/css/character.scss';
+import '@/css/profile.scss';
+import '@/css/projects.scss';
 
-  import '@/js/js.js';
-  import navbarComponent from './navbarComponent.vue';
-  import sidebarComponent from './sidebarComponent.vue';
-  import section1 from './section1/section1.vue';
-  import section2 from './section2/section2.vue';
-  import section3 from './section3/section3.vue';
+// import '@/js/js.js';
+import navbarComponent from './navbarComponent.vue';
+import section1 from './section1/section1.vue';
+import section2 from './section2/section2.vue';
+import section3 from './section3/section3.vue';
 
-  export default {
-    components: {
-      navbarComponent,
-      sidebarComponent,
-      section1,
-      section2,
-      section3
-    },
-  };
+export default {
+  components: {
+    navbarComponent,
+    section1,
+    section2,
+    section3
+  },
+};
 </script>
--- src/components/sidebarComponent.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="sidebar">
-    <a href="#section1" class="active"></a>
-    <a href="#section2"></a>
-    <a href="#section3"></a>
-  </div>
-</template>
